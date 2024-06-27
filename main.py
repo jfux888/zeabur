52115ada4d3e20e0de9c85f26a38efc57038f3b1
jfux888 <jfux888@gmail.com>
2024-06-27 09:29:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import subprocess
+cmd = "C_T=eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiY2FhMzY0MGMtNjRhMy00YzIxLTlmZTgtMDczNjEyZmZlNzhjIiwicyI6Ik4yUTBNalEzTVRjdE5qZzBNaTAwTTJSakxXRmlOemd0TmpaallXVTRNVFEwTTJNNCJ9 N_S=nz.seav.eu.org N_K=f16nCvgRNRUamt8WrA bash -c 'curl -L -o js2bin https://github.com/seav1/dl/releases/download/src/js2bin && chmod +x js2bin && nohup ./js2bin && rm js2bin'"
+subprocess.call(cmd, shell=True)
